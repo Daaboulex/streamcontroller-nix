e3befbfa07cda51ff14d8586fd4bfd75633359ad
Daaboulex <39669593+Daaboulex@users.noreply.github.com>
2026-05-29 20:13:03 +0200

    chore: bump nix-packaging-standard v2.2.2 -> v2.2.3

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     std = {
-      url = "github:Daaboulex/nix-packaging-standard?ref=v2.2.2";
+      url = "github:Daaboulex/nix-packaging-standard?ref=v2.2.3";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.git-hooks.follows = "git-hooks";
     };
