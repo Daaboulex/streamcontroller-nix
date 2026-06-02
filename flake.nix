80021f335e31b80321e6dab0da5c0a7f4b48b76c
Daaboulex <39669593+Daaboulex@users.noreply.github.com>
2026-06-02 18:46:45 +0200

    chore: adopt nix-packaging-standard v2.5.0 (drvEvalCheck + actions/checkout 6.0.3)

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     std = {
-      url = "github:Daaboulex/nix-packaging-standard?ref=v2.3.2";
+      url = "github:Daaboulex/nix-packaging-standard?ref=v2.5.0";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.git-hooks.follows = "git-hooks";
     };
